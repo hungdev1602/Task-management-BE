c4cf5718f2265a759c1ce9f0394e8ef6ff94b642
lunglinhlaplanh <158856626+hungdev1602@users.noreply.github.com>
2025-08-04 01:40:28 +0300

    Tạo task con (Lưu thêm id task cha)

--- models/task.model.js
+++ models/task.model.js
@@ -9,6 +9,7 @@ const taskSchema = mongoose.Schema(
     timeFinish: Date,
     createdBy: String,
     listUser: Array,
+    taskParentId: String,
     deleted: {
       type: Boolean,
       default: false
