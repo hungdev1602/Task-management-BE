d82e928f805ace243bc3f3c0b4203690aa7c0d6e
lunglinhlaplanh <158856626+hungdev1602@users.noreply.github.com>
2025-08-04 01:02:05 +0300

    middleware Chuyển các route thành private

---
+++ middlewares/client/user.middleware.js
@@ -0,0 +1,26 @@
+const User = require("../../models/user.model")
+
+module.exports.requireAuth = async (req, res, next) => {
+
+  const token = req.headers.authorization.split(" ")[1]
+  if(!token){
+    res.status(400).json({
+      message: "Require token"
+    })
+    return
+  }
+
+  const existUser = await User.findOne({
+    token: token,
+    deleted: false
+  })
+
+  if(!existUser){
+    res.status(400).json({
+      message: "Invalid token"
+    })
+    return
+  }
+
+  next()
+}
--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -1,8 +1,11 @@
 const routeTask = require("./task.route")
 const routeUser = require("./user.route")
+
+const userMiddleware = require("../../middlewares/client/user.middleware")
+
 module.exports = (app) => {
   
-  app.use("/tasks", routeTask)
+  app.use("/tasks", userMiddleware.requireAuth, routeTask)
 
   app.use("/users", routeUser)
 }
